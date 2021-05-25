6169306e76a1d13c6b23fb34e76f7b7b9a11604c
frida stenberg <fridastenberg97@hotmail.com>
2021-05-25 14:27:57 +0200

    created start p module

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,7 +1,11 @@
+import styles from "../css/StartPage.module.css"
+
 const StartPage = () => {
     return(
-        <div className={style.startPageWrapper}>
+        <div className={styles.startPageWrapper}>
             <h1>Start Sida</h1>
         </div>
     )
 }
+
+export default StartPage;
