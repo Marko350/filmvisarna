96405b5b9594d05f91a71ec184d0d9419770a77a
frida stenberg <fridastenberg97@hotmail.com>
2021-05-25 14:36:09 +0200

    imported hero to start p

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,9 +1,11 @@
 import styles from "../css/StartPage.module.css"
+import Hero from "../components/Hero";
 
 const StartPage = () => {
     return(
         <div className={styles.startPageWrapper}>
             <h1>Start Sida</h1>
+            <Hero />
         </div>
     )
 }
