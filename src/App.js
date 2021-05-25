11563af6ec86d4dcfa377ed7ad8f45b73eea1f31
frida stenberg <fridastenberg97@hotmail.com>
2021-05-25 14:29:42 +0200

    added start p to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import Start from './pages/Start';
 
+import StartPage from "./pages/StartPage";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route exact path='/' component={Start} />
+        {/* <Route exact path='/' component={Start} /> */}
+        <Route exact path='/' component={StartPage} />
       </BrowserRouter>
     </div>
   );
