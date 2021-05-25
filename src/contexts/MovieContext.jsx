6a3a7d6aae367fdbc7b3637dbab5bf09972f7694
Chris <to.chris.lindstrom@gmail.com>
2021-05-25 19:18:16 +0200

    Fetch movie by id

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -12,12 +12,22 @@ const MovieProvider = (props) => {
     setAllMovies(movies);
   }
 
+  // Run based on id in route on movie details page on load
+  const getMovieById = async (id) => {
+    let movie = await fetch(`/api/v1/movies/${id}`);
+    movie = await movie.json();
+    console.log(movie);
+  }
+
   useEffect(() => {
     getAllMovies();
+    getMovieById('60acacd346075c18aeee45b4');
   }, [])
 
   const values = {
-
+    getAllMovies,
+    allMovies,
+    getMovieById,
   }
 
   return ( 
