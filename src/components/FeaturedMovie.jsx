14eed90347e038ba6f48aa6810d02a1b768014bf
Pernilla <coolanilla@hotmail.com>
2021-05-25 14:30:47 +0200

    Copied files from old repo

---
+++ src/components/FeaturedMovie.jsx
@@ -0,0 +1,11 @@
+import img from '../assets/Placeholder-featuredMovie(1).jpg';
+
+function FeaturedMovie() {
+    return (
+        <div className="featured-movie">
+            <img src={img} alt="Casablanca placeholder" />
+        </div>
+    )
+}
+
+export default FeaturedMovie;
