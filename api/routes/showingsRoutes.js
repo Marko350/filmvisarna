b9437b35ee280ef78b04c847fac81b26be627c9b
Chris <to.chris.lindstrom@gmail.com>
2021-05-26 10:32:01 +0200

    Small fixes to response

--- api/routes/showingsRoutes.js
+++ api/routes/showingsRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const showingsController = require("../controllers/showingsController");
 
 router.post("", showingsController.addShowing);
-router.get("", showingsController.getShowingsByMovieAndDate);
+router.get("/movie-date", showingsController.getShowingsByMovieAndDate);
 router.get("/:showingsId", showingsController.getShowingById);
 
 module.exports = router;
