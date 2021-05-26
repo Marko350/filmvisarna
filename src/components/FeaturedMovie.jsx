41c8709162053fb8d663c116bfc4fa983e7e02cf
Pernilla <coolanilla@hotmail.com>
2021-05-26 12:17:09 +0200

    Added oneMovie state in context and used func to get casablanca movie
    In FeaturedMovie

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -1,15 +1,14 @@
 import styles from '../css/FeaturedMovie.module.css';
 import headerImg from '../assets/Placeholder-featuredMovie(1).jpg';
-import posterImg from '../assets/200px-CasablancaPoster-Gold.jpg';
 
-function FeaturedMovie() {
+function FeaturedMovie({ movie }) {
     return (
         <div className={styles.featuredMovieWrapper}>
             <img src={headerImg} alt="Casablanca placeholder" />
             <div className={styles.imgOverlay}>
-                <img src={posterImg} alt="Casablanca poster placeholer" />
+                <img src={movie.poster} alt={`${movie.title} poster`} />
                 <div className="movie-info">
-                    <p className={styles.title}>Casablanca</p>
+                    <p className={styles.title}>{movie.title}</p>
                     <p className={styles.date}>21/5 - 3/6</p>
                 </div>
             </div>
