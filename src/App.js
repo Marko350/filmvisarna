8f3e1f85b428a67ea1e687c1c6f75f4561b1bc7d
Marko <marko.zdravkovski@hotmail.com>
2021-05-26 11:01:04 +0200

    Fix in App.js
    Navbar was outside MovieProvider. I move it inside as first component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import Start from "./pages/Start";
 import Navbar from "./components/Navbar";
-import MovieProvider from './contexts/MovieContext';
-import MovieList from './pages/MovieList';
+import MovieProvider from "./contexts/MovieContext";
+import MovieList from "./pages/MovieList";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <MovieProvider>
-          <Route exact path='/' component={Start} />
-          <Route exact path='/movie-list' component={MovieList} />
+          <Navbar />
+          <Route exact path="/" component={Start} />
+          <Route exact path="/movie-list" component={MovieList} />
         </MovieProvider>
-        <Navbar />
       </BrowserRouter>
     </div>
   );
