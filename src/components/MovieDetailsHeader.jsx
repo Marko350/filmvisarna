ac52dc97ad216714eb84b00af0d905ba080defcf
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-05-26 12:48:29 +0200

    Created MovieDetailsHeader file + css

---
+++ src/components/MovieDetailsHeader.jsx
@@ -0,0 +1,11 @@
+import styles from '../css/MovieDetailsHeader.module.css';
+
+function MovieDetailsHeader() {
+    return (
+        <div className="container">
+            <div className={styles.testDiv1}>Header Test</div>
+        </div>
+    )
+}
+
+export default MovieDetailsHeader;
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,9 +1,11 @@
 import styles from '../css/MovieDetails.module.css';
+import MovieDetailsHeader from "../components/MovieDetailsHeader";
 
 function MovieDetails() {
     return (
         <div className="container">
             <div className={styles.testDiv}>TEST</div>
+            <MovieDetailsHeader></MovieDetailsHeader>
         </div>
     )
 }
