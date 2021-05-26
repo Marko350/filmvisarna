38afc64d726a7e5f65f2885f5d824f119d993a40
frida stenberg <fridastenberg97@hotmail.com>
2021-05-26 09:35:56 +0200

    added posters to start p

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,10 +1,12 @@
 import styles from "../css/StartPage.module.css"
 import Hero from "../components/Hero";
+import Posters from "../components/Poters";
 
 const StartPage = () => {
     return(
         <div className={styles.startPageWrapper}>
             <Hero />
+            <Posters />
         </div>
     )
 }
