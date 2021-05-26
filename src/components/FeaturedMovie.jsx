894f9f2b28682f188a6738be39e3b46ad1a2dc80
Pernilla <coolanilla@hotmail.com>
2021-05-26 15:15:50 +0200

    Added new headerImg and gradient

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -1,16 +1,21 @@
 import styles from '../css/FeaturedMovie.module.css';
-import headerImg from '../assets/Placeholder-featuredMovie(1).jpg';
+import headerImg from '../assets/casablanca.jpeg';
 
 function FeaturedMovie({ movie }) {
     return (
         <div className={styles.featuredMovieWrapper}>
-            <img src={headerImg} alt="Casablanca placeholder" />
-            <div className={styles.imgOverlay}>
-                <img src={movie.poster} alt={`${movie.title} poster`} />
+            <div className={styles.gradientWrapper}>
+                <img className={styles.headerImg} src={headerImg} alt="Casablanca placeholder" />
+                <div className={styles.gradient}></div>
+            </div>
+            <div className={styles.imgWrapper}>
+                <img className={styles.posterImg} src={movie.poster} alt={`${movie.title} poster`} />
                 <div className="movie-info">
                     <p className={styles.title}>{movie.title}</p>
                     <p className={styles.date}>21/5 - 3/6</p>
+                    <p className={styles.dateDesktop}>Visas hos Filmvisarna från den 21/5 till den 3/6</p>
                 </div>
+                <div className={styles.buyTickets}>Köp Biljetter</div>
             </div>
         </div>
     )
