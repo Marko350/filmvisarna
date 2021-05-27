7862401358ff077d19dec5e446e61c589bac6496
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-05-27 11:58:01 +0200

    Changed playbtn to png

--- src/components/MovieDetailsHeader.jsx
+++ src/components/MovieDetailsHeader.jsx
@@ -1,7 +1,7 @@
 import styles from '../css/MovieDetailsHeader.module.css';
 import { useContext, useEffect } from "react";
 import { MovieContext } from "../contexts/MovieContext";
-import playbtn from "../assets/play-button.svg";
+import playbtn from "../assets/playicon.png";
 
 function MovieDetailsHeader({movieId}) {
 
