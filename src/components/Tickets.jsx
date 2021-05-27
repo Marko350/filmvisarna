c2a361f003b04620bc8a9b84a75b20f8372e537e
Marko <marko.zdravkovski@hotmail.com>
2021-05-27 13:24:49 +0200

    Added style in tickets component
    Temporary function on tickets

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { container } from "../css/Tickets.module.css";
 import Ticket from "../assets/ticket.png";
-import { tickets, barnTicket } from "../css/Tickets.module.css";
+import { tickets, barnTicket, counter } from "../css/Tickets.module.css";
 
 const Tickets = () => {
-  const [number, setNumber] = useState(0);
+  const [standardTicket, setStandardTicket] = useState(0);
+  const [pensionerTicket, setPensionerTicket] = useState(0);
+  const [childTicket, setChildTicket] = useState(0);
 
   return (
     <div className={container}>
@@ -12,19 +14,47 @@ const Tickets = () => {
       <div className={tickets}>
         <p>Standard</p>
         <img src={Ticket} alt="Ticket picture" />
-        <div>
-        <i onClick={} class="fas fa-minus-circle"></i>
-        <span>{number}</span>
-        <i class="fas fa-plus-circle"></i>
+        <div className={counter}>
+          <i
+            onClick={() => setStandardTicket(standardTicket - 1)}
+            class="fas fa-minus-circle"
+          ></i>
+          <span>{standardTicket}</span>
+          <i
+            onClick={() => setStandardTicket(standardTicket + 1)}
+            class="fas fa-plus-circle"
+          ></i>
         </div>
       </div>
       <div className={tickets}>
         <p>Pensionär</p>
         <img src={Ticket} alt="Ticket picture" />
+        <div className={counter}>
+          <i
+            onClick={() => setPensionerTicket(pensionerTicket - 1)}
+            class="fas fa-minus-circle"
+          ></i>
+          <span>{pensionerTicket}</span>
+          <i
+            onClick={() => setPensionerTicket(pensionerTicket + 1)}
+            class="fas fa-plus-circle"
+          ></i>
+        </div>
       </div>
       <div className={barnTicket}>
         <p>Barn</p>
         <img src={Ticket} alt="Ticket picture" />
+        <div className={counter}>
+          <i
+            onClick={() => setChildTicket(childTicket - 1)}
+            class="fas fa-minus-circle"
+          ></i>
+          <span>{childTicket}</span>
+          <i
+            onClick={() => setChildTicket(childTicket + 1)}
+            class="fas fa-plus-circle"
+          ></i>
+        </div>
       </div>
     </div>
   );
