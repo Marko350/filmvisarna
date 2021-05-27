766f6bdd311ef70f7642c5997502ad8e6ebabceb
Marko <marko.zdravkovski@hotmail.com>
2021-05-27 10:38:31 +0200

    ticket

--- src/components/Tickets.jsx
+++ src/components/Tickets.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { container } from "../css/Tickets.module.css";
 import Ticket from "../assets/ticket.png";
 import { tickets, barnTicket } from "../css/Tickets.module.css";
 
 const Tickets = () => {
+  const [number, setNumber] = useState(0);
+
   return (
     <div className={container}>
       <h1>Välj antal biljetter</h1>
       <div className={tickets}>
         <p>Standard</p>
         <img src={Ticket} alt="Ticket picture" />
-        <i class="fas fa-minus-circle"></i>
+        <div>
+        <i onClick={} class="fas fa-minus-circle"></i>
+        <span>{number}</span>
+        <i class="fas fa-plus-circle"></i>
+        </div>
       </div>
       <div className={tickets}>
         <p>Pensionär</p>
