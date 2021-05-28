8239e0a9d7e90c4ad0dbc4b41df3e9c329bdb5f6
Pernilla <coolanilla@hotmail.com>
2021-05-28 15:01:02 +0200

    Added heading, datepicker and basic styling on Showings component

--- src/components/Showings.jsx
+++ src/components/Showings.jsx
@@ -1,6 +1,22 @@
+import styles from '../css/Showings.module.css';
+
 function Showings() {
+
+    const date = new Date().toISOString().slice(0, 10);
+
     return (
-        <h1>Showings works!</h1>
+        <div className="showings">
+            <div className={styles.datepickerLine}>
+                <h2>Date</h2>
+                <input
+                    type="date"
+                    id="date"
+                    name="date"
+                    defaultValue={date}
+                    min={date}
+                    max="2021-12-31" />
+            </div>
+        </div>
     )
 }
 
