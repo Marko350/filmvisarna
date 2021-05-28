b9a430c5024b4ac01f77a6254552f14438829a31
frida stenberg <fridastenberg97@hotmail.com>
2021-05-28 10:17:25 +0200

    working on it

--- src/components/Posters.js
+++ src/components/Posters.js
@@ -14,7 +14,7 @@ const Posters = () => {
                         <div className={styles.posterContainerOne}>
                             <img className={styles.imgOne} src={today[0].temp[0].movieId[0].poster} alt="poster" />
                         </div>
-                        <div className={styles.posterContainerTwo }>
+                        <div className={styles.posterContainerTwo}>
                             <img className={styles.imgTwo} src={today[0].temp[1].movieId[0].poster} alt="poster"  />
                             <img className={styles.imgThree} src={today[0].temp[0].movieId[0].poster} alt="poster"  />
                         </div>
@@ -23,7 +23,6 @@ const Posters = () => {
                         {today.map((t, i) => (
                             <div className={styles.margin} key={i}>
                                 <h2>{t.time}</h2>
-
                             </div>
                         ))}
                     </div>
