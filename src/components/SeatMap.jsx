9a9232ddcc4bf2dd57fb34e2baa6f0b432ec61db
Chris <to.chris.lindstrom@gmail.com>
2021-05-28 14:25:00 +0200

    Initial file setup

---
+++ src/components/SeatMap.jsx
@@ -0,0 +1,11 @@
+import style from '../css/SeatMap.module.css';
+
+const SeatMap = () => {
+  return ( 
+    <div className={style.seatMapWrapper}> 
+
+    </div>
+   );
+}
+ 
+export default SeatMap;
