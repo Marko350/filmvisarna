265f643df1d0a03e2c84bb778d3c7ac152b7cf86
Chris <to.chris.lindstrom@gmail.com>
2021-05-28 20:32:45 +0200

    Render booked and chosen seats css

--- src/components/SeatMap.jsx
+++ src/components/SeatMap.jsx
@@ -1,20 +1,33 @@
+import { useState } from 'react';
 import style from '../css/SeatMap.module.css';
 
 const SeatMap = () => {
+  const [chosenSeats, setChosenSeats] = useState([]);
+  const [bookedSeats, setBookedSeats] = useState(['a1', 'a2', 'a3', 'b2', 'b3', 'c4', 'c5'])
   let rows = ['a', 'b', 'c', 'd'];
   let seatsPerRow = 8;
-  let bookedSeats = ['a1', 'a2', 'a3', 'b2', 'b3', 'c4', 'c5'];
+
+
+  const handleSeatClick = (e) => {
+    let seat = e.target.getAttribute('data-seat');
+    setChosenSeats([...chosenSeats, seat]);
+    console.log(seat)
+  }
 
   let seatsContent = 'Laddar...';
   if (rows) {
     let allSeatsHtml = rows.map((row, index) => {
-      let rowSeats = '';
-      for (let i = 1; i < seatsPerRow; i++) {
-        rowSeats += `<span data-seat=${row}${i}>${i}</span>`
+      let rowSeats = [];
+      for (let i = 1; i <= seatsPerRow; i++) {
+        let seatClass = 
+          bookedSeats.includes(`${row}${i}`) && style.booked || chosenSeats.includes(`${row}${i}`) && style.chosen;
+          
+        rowSeats.push
+          (<div key={i} className={`${style.seat} ${seatClass}`} onClick={handleSeatClick} data-seat={`${row}${i}`}>{i}</div>)
       }
-      return `<div><h3>${row}</h3>${rowSeats}</div>`
+      return <div className={style.seatRow} key={index}>{rowSeats}<div>{row}</div></div>
     })
-    seatsContent = <h3>Hej</h3>;
+    seatsContent = allSeatsHtml;
   }
 
   return ( 
