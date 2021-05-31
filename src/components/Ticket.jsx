d7a4c980243c2e1f31afe9c6cd92e17f4c08d936
Chris <to.chris.lindstrom@gmail.com>
2021-05-31 12:54:25 +0200

    Displaying correct info on ticket

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,4 +1,6 @@
+import { useContext } from 'react';
 import TicketPicture from "../assets/ticket-bg.png";
+import { MovieContext } from '../contexts/MovieContext';
 import {
   container,
   movieInfo,
@@ -6,7 +8,9 @@ import {
   imageContainer,
 } from "../css/Ticket.module.css";
 
-const Ticket = () => {
+const Ticket = ({ showing }) => {
+  const { tickets } = useContext(MovieContext);
+  console.log('showing in ticket', showing);
 
   return (
     <div className={container}>
@@ -14,17 +18,18 @@ const Ticket = () => {
       <div className={ticketInfo}>
         <div className={imageContainer}>
           <img
-            src="https://m.media-amazon.com/images/M/MV5BY2IzZGY2YmEtYzljNS00NTM5LTgwMzUtMzM1NjQ4NGI0OTk0XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg"
+            // src="https://m.media-amazon.com/images/M/MV5BY2IzZGY2YmEtYzljNS00NTM5LTgwMzUtMzM1NjQ4NGI0OTk0XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_SX300.jpg"
+            src={showing.movieId[0].poster}
             alt="Poster"
           />
         </div>
         <div className={movieInfo}>
-          <span>Star Wars</span>
-          <span>03/05/2022 00:00</span>
-          <span>Standard: 2</span>
-          <span>Pensionär: 1</span>
-          <span>Barn: 1</span>
-          <span>Totalt pris: 250kr</span>
+          <span>{showing.movieId[0].title}</span>
+          <span>{showing.date} - {showing.time}</span>
+          { tickets.standard > 0 && <span>Standard: {tickets.standard}</span> }
+          { tickets.senior > 0 && <span>Pensionär: {tickets.senior}</span> }
+          { tickets.child > 0 && <span>Barn: {tickets.child}</span> }
+          <span>Totalt pris: {tickets.totalPrice} kr</span>
         </div>
       </div>
       <img src={TicketPicture} alt="Ticket" />
