d9934b48722878b38343a188246602fb37465fce
Pernilla <coolanilla@hotmail.com>
2021-05-31 13:31:07 +0200

    Small merge fix

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -5,6 +5,7 @@ import { useContext, useEffect } from "react";
 import { MovieContext } from "../contexts/MovieContext";
 
 function MovieList() {
+
   const { getMovieById, movieById } = useContext(MovieContext);
 
   useEffect(() => {
@@ -12,28 +13,20 @@ function MovieList() {
     // eslint-disable-next-line
   }, []);
 
-    useEffect(() => {
-        getMovieById("60b49a37b62f8359984329e7");
-        // eslint-disable-next-line
-    }, []);
-
-    return (
-        <div className="container">
-            <div className="header">
-                <h2 className={styles.heading}>I fokus</h2>
-                {movieById && <FeaturedMovie movie={movieById} />}
-            </div>
-            <div className="movie-list">
-                <h2 className={styles.heading}>Alla filmer</h2>
-                <div className={styles.filterLine}>
-                    <h3>På bio nu</h3>
-                    <p>Visa filter</p> {/* Make onClick when filter component is done */}
-                </div>
-                <MovieCardList />
-            </div>
+  return (
+    <div className="container">
+        <div className="header">
+            <h2 className={styles.heading}>I fokus</h2>
+            {movieById && <FeaturedMovie movie={movieById} />}
         </div>
-        <MovieCardList />
-      </div>
+        <div className="movie-list">
+            <h2 className={styles.heading}>Alla filmer</h2>
+            <div className={styles.filterLine}>
+                <h3>På bio nu</h3>
+                <p>Visa filter</p> {/* Make onClick when filter component is done */}
+            </div>
+            <MovieCardList />
+        </div>
     </div>
   );
 }
