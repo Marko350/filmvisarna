a6124ff9f6576f9102b4c7ab77ae4bf81fc483b8
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-05-31 15:40:11 +0200

    Added placeholder text + background

--- src/components/ConfirmationPageInfo.jsx
+++ src/components/ConfirmationPageInfo.jsx
@@ -3,8 +3,12 @@ import styles from "../css/ConfirmationPage.module.css";
 function ConfirmationPageInfo() {
 
     return (
-        <div className="container">
-            <p>TEST 2</p>
+        <div className={styles.confirmationBox}>
+            <h3>FilmRubrik och tid 00000</h3>
+            <p>Bokningsnr. 000000</p>
+            <p>Stolar: A12, A13, A14</p>
+            <br></br>
+            <p>Summa: 100SEK</p>
         </div>
     )
 }
