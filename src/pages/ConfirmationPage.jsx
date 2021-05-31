50369ee6b40f78419c1ff11714d21d3466dce739
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-05-31 15:06:49 +0200

    Added styling to confirmation text

--- src/pages/ConfirmationPage.jsx
+++ src/pages/ConfirmationPage.jsx
@@ -5,7 +5,7 @@ function ConfirmationPage() {
     return (
         <div className="container">
             <div className={styles.textContainer}>
-                <h1>TACK!</h1>
+                <h1 className={styles.confirmationH1} >TACK!</h1>
                 <p>Din bokning var framgångsrik.</p>
             </div>
         </div>
