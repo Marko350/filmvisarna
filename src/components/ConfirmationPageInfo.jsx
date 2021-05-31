ab06303ccad3dc6c4bf96084981067e6f4c0b59d
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-05-31 15:56:35 +0200

    Added placeholder poster + basic flex css

--- src/components/ConfirmationPageInfo.jsx
+++ src/components/ConfirmationPageInfo.jsx
@@ -3,12 +3,15 @@ import styles from "../css/ConfirmationPage.module.css";
 function ConfirmationPageInfo() {
 
     return (
-        <div className={styles.confirmationBox}>
-            <h3>FilmRubrik och tid 00000</h3>
-            <p>Bokningsnr. 000000</p>
-            <p>Stolar: A12, A13, A14</p>
-            <br></br>
-            <p>Summa: 100SEK</p>
+        <div className={styles.confirmationWrapper} >
+            <div className={styles.confirmPoster}>Placeholder image</div>
+            <div className={styles.confirmationBox}>
+                <h3>FilmRubrik och tid 00000</h3>
+                <p>Bokningsnr. 000000</p>
+                <p>Stolar: A12, A13, A14</p>
+                <br></br>
+                <p>Summa: 100SEK</p>
+            </div>
         </div>
     )
 }
