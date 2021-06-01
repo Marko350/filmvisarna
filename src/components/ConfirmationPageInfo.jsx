ce0be0f41de6921169b6c7abb33db7820b732f6c
Lovisa Nordström <lovisa.nordstroms@gmail.com>
2021-06-01 14:58:23 +0200

    added container div

--- src/components/ConfirmationPageInfo.jsx
+++ src/components/ConfirmationPageInfo.jsx
@@ -4,22 +4,24 @@ import { MovieContext } from "../contexts/MovieContext";
 function ConfirmationPageInfo({bookedTicket}) {
 
     return (
-        <div className={styles.confirmationWrapper} >
-            <img
-                src={bookedTicket.poster}
-                alt={bookedTicket.movieTitle}
-                className={styles.confirmPoster}
-            />
-            <div className={styles.confirmationInfo}>
-                <div className={styles.confirmationBox}>
-                    <h3 className={styles.confirmHeading}>{bookedTicket.movieTitle}, {bookedTicket.time}</h3>
-                    <p className={styles.confirmP}>(Bokningsnr. 000000)</p>
-                    <p className={styles.confirmP}>Stolar: {bookedTicket.seats}</p>
-                    <br></br>
-                    <p className={styles.confirmTotal}>Summa: {bookedTicket.ticketTypes.totalPrice} :- </p>
-                </div>
-                <div className={styles.findOrderHistory}>
-                    <p className={styles.findOrderHistoryText}>Du hittar alltid din orderhistorik under Mina sidor. </p>
+        <div className="container">
+            <div className={styles.confirmationWrapper} >
+                <img
+                    src={bookedTicket.poster}
+                    alt={bookedTicket.movieTitle}
+                    className={styles.confirmPoster}
+                />
+                <div className={styles.confirmationInfo}>
+                    <div className={styles.confirmationBox}>
+                        <h3 className={styles.confirmHeading}>{bookedTicket.movieTitle}, {bookedTicket.time}</h3>
+                        <p className={styles.confirmP}>(Bokningsnr. 000000)</p>
+                        <p className={styles.confirmP}>Stolar: {bookedTicket.seats}</p>
+                        <br></br>
+                        <p className={styles.confirmTotal}>Summa: {bookedTicket.ticketTypes.totalPrice} :- </p>
+                    </div>
+                    <div className={styles.findOrderHistory}>
+                        <p className={styles.findOrderHistoryText}>Du hittar alltid din orderhistorik under Mina sidor. </p>
+                    </div>
                 </div>
             </div>
         </div>
