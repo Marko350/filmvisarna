5fc35ab445f9681473089dfe5b3ab1d5f213f6f3
frida stenberg <fridastenberg97@hotmail.com>
2021-06-01 14:46:36 +0200

    route template

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,19 @@
 import styles from "../css/HeroComp.module.css";
 import Btn from "../assets/buttonImg/btn-small.png";
+import { useHistory } from "react-router-dom";
 
 const Hero = () => {
+    const historyHook = useHistory();
+
+    const clickToRender = () => {
+        historyHook.push()
+    }
     return(
         <div className={styles.heroWrapper}>
             <div className={styles.heroGreeting}>
                 <h1>Välj bland ett urval av gamla godingar</h1>
             </div>
-            <div className={styles.btnContainer}>
+            <div className={styles.btnContainer} onClick={clickToRender()}>
                 <div className={styles.btn}>
                     <p>Välj film</p>
                     <img src={Btn} alt="Button" />
